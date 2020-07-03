908503116c4f17f64f27d3b54fe7351b3a174140
rdcgomez <dev.renzgomez@gmail.com>
2020-07-03 17:19:14 +0800

    FIX: change to sass

--- job/webpack.mix.js
+++ job/webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
     mix.js('src/js/app.js', 'public/js/app.js')
-   .less('src/scss/app.less', 'public/css/app.css')
+   .sass('src/scss/app.scss', 'public/css/app.css')
    .copy('src/fonts', 'public/fonts')
    .copy('src/images', 'public/images')
    .options({
