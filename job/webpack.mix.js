71a2dfef12227b4776c7c881901985c36608f08a
rdcgomez <dev.renzgomez@gmail.com>
2020-07-03 17:02:37 +0800

    FIX: less to scss

--- job/webpack.mix.js
+++ job/webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
     mix.js('src/js/app.js', 'public/js/app.js')
-   .less('src/less/app.less', 'public/css/app.css')
+   .less('src/scss/app.less', 'public/css/app.css')
    .copy('src/fonts', 'public/fonts')
    .copy('src/images', 'public/images')
    .options({
